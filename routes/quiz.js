6f91a11227c8cd98b3258cc679090e808612247c
jaythomasv29 <jaythomasv29@gmail.com>
2019-11-06 16:04:01 -0500

    added quiz route that retrieves JSON data

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,31 +1,20 @@
-
 var express = require('express');
-//added axios for api calls
 var axios = require('axios')
 var router = express.Router();
 
-let data;
-/* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send(data);
-
-  
+  let category = req.query.category
+  quizSelection(category).then(data => res.send(data))
 });
 
-//Get selection based on users selection
-  // let selection = req.body.category
-  let url = `http://localhost:3000/api`
-  //api call
-   axios.post(url, {
-    category: '1'
+let url = `http://localhost:3000/api`
+let quizSelection = async (category) => {
+
+  let response = await axios.post(url, {
+    category: category
   })
-  .then(function (response) {
-    data = response
-    console.log(data)
-    console.log(data.results, 'data.results')
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+
+  return response.data.results;
+}
 
 module.exports = router;
